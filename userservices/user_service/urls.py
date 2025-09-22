5e6cda2da0bf6ecd4c4c11b665f8a7642cb5268b
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2025-09-22 09:58:45 +0530

    Update urls.py

--- userservices/user_service/urls.py
+++ userservices/user_service/urls.py
@@ -1,1 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from users.views import register_user
 
+urlpatterns = [
+path('admin/', admin.site.urls),
+path('auth/register/', register_user),
+]
